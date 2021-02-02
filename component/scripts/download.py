173ea0cdd7a15a5d4b861e4d70b94f0d04a751f8
Pierrick Rambaud <pierrick.rambaud49@gmail.com>
2021-02-02 17:21:55 +0000

    add 0 as no-data in the files

--- component/scripts/download.py
+++ component/scripts/download.py
@@ -35,7 +35,8 @@ def digest_tiles(aoi_io, filename, result_dir, output, tmp_file):
 
     data, output_transform = merge(sources)
     
-    out_meta = sources[0].meta.copy() 
+    out_meta = sources[0].meta.copy()
+    out_meta.update(nodata=0)
     out_meta.update(
         driver    = "GTiff",
         height    =  data.shape[1],
