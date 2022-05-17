fff8269feec42b0279c60415e2822e0371953f42
Amit Ghosh <ghoshamitag@gmail.com>
2022-05-17 20:20:37 +0000

    enhance: improve the arangeemts of the advanced parameters

---
+++ component/widget/bool_question.py
@@ -0,0 +1,25 @@
+from traitlets import link, CBool, Int
+import ipyvuetify as v
+import sepal_ui.sepalwidgets as sw
+
+
+class BoolQuestion(v.Flex, sw.SepalWidget):
+
+    v_model = CBool(False).tag(sync=True)
+
+    def __init__(self, question, *args, **kwargs):
+
+        super().__init__(*args, **kwargs)
+
+        bool_radios = v.RadioGroup(
+            row=True,
+            v_model=self.v_model,
+            children=[
+                v.Radio(label="No", value=False),
+                v.Radio(label="Yes", value=True),
+            ],
+        )
+
+        link((bool_radios, "v_model"), (self, "v_model"))
+
+        self.children = [v.Html(tag="h3", children=[question]), bool_radios]
